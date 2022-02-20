74d9b7513335515e3792aa2d463c56de8973bcc4
sthossan <38519655+sthossan@users.noreply.github.com>
2022-02-20 12:55:51 +0600

    Update rank_denserank_rownumber.sql

--- rank_denserank_rownumber.sql
+++ rank_denserank_rownumber.sql
@@ -1,3 +1,33 @@
+CREATE Database ShowRoom;
+GO
+USE ShowRoom;
+
+CREATE TABLE Cars
+(
+id INT,
+name VARCHAR(50) NOT NULL,
+company VARCHAR(50) NOT NULL,
+power INT NOT NULL
+)
+
+USE ShowRoom
+INSERT INTO Cars
+VALUES(1, 'Corrolla', 'Toyota', 1800),
+	(2, 'City', 'Honda', 1500),
+	(3, 'C200', 'Mercedez', 2000),
+	(4, 'Vitz', 'Toyota', 1300),
+	(5, 'Baleno', 'Suzuki', 1500),
+	(6, 'C500', 'Mercedez', 5000),
+	(7, '800', 'BMW', 8000),
+	(8, 'Mustang', 'Ford', 5000),
+	(9, '208', 'Peugeot', 5400),
+	(10, 'Prius', 'Toyota', 3200),
+	(11, 'Atlas', 'Volkswagen', 5000),
+	(12, '110', 'Bugatti', 8000),
+	(13, 'Landcruiser', 'Toyota', 3000),
+	(14, 'Civic', 'Honda', 1800),
+	(15, 'Accord', 'Honda', 2000)
+
 SELECT name,company, power
 	, RANK() OVER(ORDER BY power DESC) AS [Rank]
 	, DENSE_RANK() OVER(ORDER BY power DESC) AS [Dense Rank]
